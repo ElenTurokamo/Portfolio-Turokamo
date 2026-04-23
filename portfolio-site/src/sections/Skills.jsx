b22ae558bc5b589ab6a7869a7aaa3d574a505ca6
Turokamo Elen <ElenNightForewer@gmail.com>
2026-04-23 11:22:08 +0500

    Feature: Skills section CSS

--- portfolio-site/src/sections/Skills.jsx
+++ portfolio-site/src/sections/Skills.jsx
@@ -1,36 +1,75 @@
 function Skills() {
     return (
-        <section>
-            <h1>Skill & Technologies</h1>
-            <div>
-                <h3>IT Specialist</h3>
-                <div>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
+        <section id="skills" className="skills">
+            <h1 className="skills-title">Skill & Technologies</h1>
+            <div className="skills-grid">
+                
+                <div className="skill-stacks">
+                    <h3 className="skill">Frontend</h3>
+                    <div className="skill-stack">
+                        <span className="stack">React</span>
+                        <span className="stack">CSS</span>
+                        <span className="stack">JavaScript</span>
+                        <span className="stack">HTML</span>
+                        <span className="stack">Bootstrap</span>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <h3>IT Specialist</h3>
-                <div>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
+
+                <div className="skill-stacks">
+                    <h3 className="skill">IT Specialist</h3>    
+                    <div className="skill-stack">
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <h3>IT Specialist</h3>
-                <div>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
-                    <span>Service</span>
+
+                <div className="skill-stacks">
+                    <h3 className="skill">IT Specialist</h3>
+                    <div className="skill-stack">
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                    </div>
                 </div>
+
+                <div className="skill-stacks">
+                    <h3 className="skill">IT Specialist</h3>
+                    <div className="skill-stack">
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                    </div>
+                </div>
+
+                <div className="skill-stacks">
+                    <h3 className="skill">IT Specialist</h3>    
+                    <div className="skill-stack">
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                    </div>
+                </div>
+
+                <div className="skill-stacks">
+                    <h3 className="skill">IT Specialist</h3>
+                    <div className="skill-stack">
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                        <span className="stack">Service</span>
+                    </div>
+                </div>
+
             </div>
         </section>
     )
